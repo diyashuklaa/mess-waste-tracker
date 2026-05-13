321458c272457aaec9a3f78a360af3f26d42c299
Diya Shukla <diyashukla407@gmail.com>
2026-05-13 06:47:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -236,5 +236,5 @@ def weekly():
         """).fetchall()
     return jsonify([dict(r) for r in rows])
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
